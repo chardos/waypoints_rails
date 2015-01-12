d8cf8210b47d8e4429ff0c5dfcd03f394954f5a3
Richard Tan <chardos@gmail.com>
2015-01-12 13:35:35 +1100

    Initial commit

---
+++ lib/waypoint_rails.rb
@@ -0,0 +1,9 @@
+require "waypoint_rails/version"
+
+module WaypointRails
+  module Rails
+    class Engine < ::Rails::Engine
+
+    end
+  end
+end
---
+++ lib/waypoint_rails/version.rb
@@ -0,0 +1,3 @@
+module WaypointRails
+  VERSION = "3.0.0"
+end
